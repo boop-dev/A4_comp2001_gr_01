a8c9e5a9eff51b0f730751f264f22d0b84e9548e
keagan crane <kjcrane@mun.ca>
2022-03-28 14:28:16 -0230

    game and simulation

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+/**
+ * Write a description of class Game here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Game
+{
+    
+    /**
+     * Constructor for objects of class Game
+     */
+    public Game()
+    {
+
+    }
+}
--- Team.java
+++ Team.java
@@ -1,4 +1,4 @@
-
+import  java.util.ArrayList;
 /**
  * Write a description of class Team here.
  *
@@ -18,5 +18,39 @@ public class Team
         name = name;
         group = group;
     }
-
+    
+    public void teamNames(ArrayList<Team> teamsList) {
+        Team team = new Team("USA", 1);
+        teamsList.add(team);
+        team = new Team("Uruguay", 1);
+        teamsList.add(team);
+        team = new Team("Germany", 1);
+        teamsList.add(team);
+        team = new Team("Mexico", 1);
+        teamsList.add(team);
+        team = new Team("Belgium", 2);
+        teamsList.add(team);
+        team = new Team("England", 2);
+        teamsList.add(team);
+        team = new Team("Italy", 2);
+        teamsList.add(team);
+        team = new Team("Denmark", 2);
+        teamsList.add(team);
+        team = new Team("Brazil", 3);
+        teamsList.add(team);
+        team = new Team("Spain", 3);
+        teamsList.add(team);
+        team = new Team("Portugal", 3);
+        teamsList.add(team);
+        team = new Team("Netherlands", 3);
+        teamsList.add(team);
+        team = new Team("France", 4);
+        teamsList.add(team);
+        team = new Team("Croatia", 4);
+        teamsList.add(team);
+        team = new Team("Switzerland", 4);
+        teamsList.add(team);
+        team = new Team("Argentina", 4);
+        teamsList.add(team);
+    }
 }
