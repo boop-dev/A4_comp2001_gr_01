d17d7384a0bef9c2ede66df014c96e2441f9e249
keagan crane <kjcrane@mun.ca>
2022-03-28 14:12:33 -0230

    Initial sharing of project

---
+++ Team.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Team here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Team
+{
+    private String name;
+    private int group;
+    
+    /**
+     * Constructor for objects of class Team
+     */
+    public Team(String name,int group)
+    {
+        name = name;
+        group = group;
+    }
+
+}
